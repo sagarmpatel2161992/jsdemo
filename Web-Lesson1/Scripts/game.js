8ef85546de4bc5500687b1faabbe06000ea80caf
sagarmpatel2161992 <sagarmpatel2161992@yahoo.com>
2015-01-15 10:38:46 -0800

    added

---
+++ Web-Lesson1/Scripts/game.js
@@ -0,0 +1,23 @@
+﻿var canvas;
+var stage;
+var hellotext;
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameloop);
+    main();
+}
+
+function gameloop() {
+    hellotext.x++;
+    hellotext.y++;
+    stage.update();
+}
+
+function main() {
+    hellotext = new createjs.Text("Hello World", "40px Consolas", "#000000");
+    stage.addChild(hellotext);
+}
+//# sourceMappingURL=game.js.map
